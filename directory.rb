f3363c0f72c43da1f18185904c246c3786dc36ae
olwend <olwend@gmail.com>
2016-12-11 21:00:29 +0000

    Ex11 Save to students.csv

--- directory.rb
+++ directory.rb
@@ -27,6 +27,7 @@ def interactive_menu
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
     puts "9. Exit"  #9 because we'll be adding more items
 end
 
@@ -36,6 +37,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -47,6 +50,18 @@ def show_students
   print_header
   print_index
   print_footer
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 #use rstrip to get rid of space?
 def input_students
