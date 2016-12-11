73be9cc8525ead8b1a97f915b34af3333a3e3d14
olwend <olwend@gmail.com>
2016-12-11 17:22:12 +0000

    Ex8 with fixes

--- directory.rb
+++ directory.rb
@@ -3,10 +3,10 @@
 # let's put all the students into an array
 
 @students = [
-  #{name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "cooking", country: :Wales, height:"185 cm"},
-  #{name: "Darth Vader", cohort: :november,hobbies: "cooking", country: :France, height: "185 cm"},
-  #{name: "Nurse Ratched", cohort: :january,hobbies: "cooking", country: :Eire, height: "185 cm"},
-  #{name: "Michael Corleone", cohort: :march,hobbies: "cooking", country: :USA, height: "185 cm"},
+  {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "cooking", country: :Wales, height:"185 cm"},
+  {name: "Darth Vader", cohort: :november,hobbies: "cooking", country: :France, height: "185 cm"},
+  {name: "Nurse Ratched", cohort: :january,hobbies: "cooking", country: :Eire, height: "185 cm"},
+  {name: "Michael Corleone", cohort: :march,hobbies: "cooking", country: :USA, height: "185 cm"},
   {name: "Alex DeLarge", cohort: :march},
   {name: "The Wicked Witch of the West", cohort: :november},
   {name: "Terminator", cohort: :november},
@@ -62,38 +62,41 @@ def print_body(students)
 end
 
 #1.amend to add a number
-def print_index(students)
-  students.each_with_index do |student,index|
-    puts "#{index}.  #{student[:name]}".ljust(3)
-    puts "(#{ student[:cohort]} cohort) #{student[:height]} Height Residency:#{student[:country]}".center(40)
+def print_index
+  if @students.length > 0
+     @students.each_with_index do |student,index|
+          puts "#{index}.  #{student[:name]}".ljust(3)
+          puts "(#{ student[:cohort]} cohort) #{student[:height]} Height Residency:#{student[:country]}".center(40)
   end
 end
+
 #2.create alternative print for specific letter
-def print_specific_students(students)
+def print_specific_students
   puts "Which letter student do you want?"
   l = gets.chomp
   # could use string.start_with?()
   #create new hash as a subset based on specific letter
-  set_students = @students.select {|name:,cohort:,hobbies:,height,:country| name.start_with?(l)}
+  set_students = @students.select {|student|  student[:name].start_with?(l)}
+  p set_students
   set_students.each_with_index do |student,index|
     puts "#{index}.#{student[:name]}(#{ student[:cohort]} cohort)".center(40)
    end
 end
 
 #3. name.length < 12 chars
-def print_less_twelve(students)
-  set_students = @students.select {|name:,cohort:| name.length < 12}
+def print_less_twelve
+  set_students = @students.select {|student|  student[:name].length < 12}
   set_students.each_with_index do |student,index|
     puts "#{index}.#{student[:name]}(#{ student[:cohort]} cohort)#{student[:height]} Height Residency:#{student[:country]}".center(40)
    end
 end
 #4.convert to use a loop
-def loop_body(students)
+def loop_body
     i = 0
-    while i < students.length
+    while i < @students.length
       #use your count function to count though the index numbers from 0
       #use index to pull name and cohort out of array
-          array = students[i] #gives {:name=>"Olwen", :cohort=>"January"}
+          array = @students[i] #gives {:name=>"Olwen", :cohort=>"January"}
           puts array
           i +=1
     end
@@ -102,35 +105,42 @@ end
 #Put into a hash which can be used to print
 
 #def print_by_cohort
-  #sort_months_list
-  #  students_by_cohort = []
-  #    @Months.each_with_index do |month, index|
-  #      students_by_cohort << @students.select{|student| student[:cohort]== month.to_sym}
-  #    end
-  #    puts students_by_cohort
-      #  students_by_cohort.each_with_index do |student,index|
-    #puts "#{index}.  #{student[:name]}".ljust(3)
-    #puts "(#{ student[:cohort]} cohort) #{student[:height]} Height Residency:#{student[:country]}".center(40)
-  #end
-#end
+def sort_months_list
+   students_by_cohort = []
+     @Months.each_with_index do |month, index|
+        students_by_cohort << @students.select{|student| student[:cohort]== month.to_sym}
+     end
+#by name
+     students_by_cohort.map! do |month|
+       month.sort{|a,b| a[:name] <=> b[:name]}
+     end
+#rebuild list
+     @students = []
+     @Months.each_with_index do |month,index|
+       @students << students_by_cohort[index]
+     end
+     @students.flatten!
+     puts @students
+  end
+end
 #finally, we print the total number of students
-def print_footer(students)
-  case students.length
+def print_footer
+  case @students.length
   when 1 then puts "We have 1 great student"
   when 0 then puts "We have no students?"
-  else puts "Overall, we have #{students.count} great students"
+  else puts "Overall, we have #{@students.count} great students"
   end
 end
 
-
+#10. Try "string\n\r".chop   #=> "string\n"
 
 #nothing happens until we call the methods
 #students = input_students
 print print_header
-#print print_body(students)
-#print print_by_cohort
-print print_index(@students)
-print print_specific_students(@students)
-#print print_less_twelve(@students)
+#print print_body
+print print_index
+print print_specific_students
+print print_less_twelve
 #print loop_body(students)
-print print_footer(@students)
+print sort_months_list
+print print_footer
